efad26c53030094bf2179313e952c7b816ddab3d
jacek rostocki <jacekrostocki7@gmail.com>
2022-12-12 11:42:53 +0100

    qq added

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,36 @@
 const survey = {
-  intro: [],
+  intro: [
+    "Jestes Katolikiem?",
+    ["o", "Tak", "Nie"],
+    "Gotowy na szybki test wiary?",
+    ["o", "Tak", "Jasne, ze tak!"],
+    "Noo tooo, lecimyyyy ;0!!",
+  ],
+  stage1: [
+    "Czy wiesz gdzie w Biblii zawarte jest 10 przykazan?",
+    [
+      "o",
+      "Nowy Testament, ew. Jana?",
+      "Stary Testament ks Wyjscia rodzial 8 (od wersu 4)?",
+    ],
+    [
+      "r",
+      "Upsik! Są opisane w ks. Wyjscia., ale nic się nie martw bo i tak lecimy dalej! No i poczytaj ew. Jana bo wyrywa z butów :D!",
+      "Brawo! Lecimy zatem dalej!",
+    ],
+    "Teraz, potrzebujemy abys przeczytal tresc 2 przykazania w biblii (Ks. Wyjscia rozdzial 20 wersy: 4-6). Twoje wnioski, przemyslenia sa kluczowe w nastepnym etapie. ",
+    "Wnioski: przykazania orpacowane przez kosciol Rzymsko-Katolicki (wy-googluj dla porownania ;)) róźnią sie od tych biblijnych",
+  ],
+  stage2: [
+    "Co myslisz na ten temat?",
+    ["o/P", "Jest to detal", "Nie wiedziałem/am"],
+  ],
+  stageP1: [
+    "Przeczytaj to i wroc zeby odpowiedz dalej: List do Galacjan rozdzial 1 wersy: 6-9 (15 sekund czytania :)). Czy ma to jakis wplyw na Ciebie? Refleksje?",
+
+    ["o", "Zerowy wpływ. Papiez wie co robi a ja chodze na msze itp", ""],
+    "",
+    ["", "", ""],
+  ],
+  stageP2: ["", ["", "", ""], "", ["", "", ""]],
 };
